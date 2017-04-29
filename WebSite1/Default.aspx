309c84c0397d4b2878b37f423b2e2b9a7fd79e18
TheOneGera <gerardoantoniomtz@hotmail.com>
2017-04-29 11:19:20 -0500

    Agregando primer proyecto .NET

---
+++ WebSite1/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 109px;
+        }
+    </style>
+</head>
+<body>
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:TextBox ID="TextBox1" runat="server" Height="21px"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Hacer la operacion" />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:Label ID="Label2" runat="server"></asp:Label>
+        <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" style="margin-bottom: 0px">
+            <asp:ListItem>Suma</asp:ListItem>
+            <asp:ListItem Value="Resta"></asp:ListItem>
+            <asp:ListItem Value="Multiplicacion"></asp:ListItem>
+            <asp:ListItem Value="divisiones">divisiones</asp:ListItem>
+        </asp:DropDownList>
+        <br/>
+        <asp:Label ID="Label4" runat="server"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Hacerel pino" />
+        <asp:DropDownList ID="DropDownList2" runat="server">
+            <asp:ListItem>+</asp:ListItem>
+            <asp:ListItem>*</asp:ListItem>
+        </asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.Label1.Text = "Hello FIme";
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+       
+
+        if (DropDownList1.SelectedValue == "Suma")
+        {
+            int num1 = Convert.ToInt32(this.TextBox1.Text);
+            int num2 = Convert.ToInt32(this.TextBox2.Text);
+            int sum = num1 + num2;
+            this.Label2.Text = sum.ToString();
+        }
+        if (DropDownList1.SelectedValue == "Resta")
+        {
+            int num1 = Convert.ToInt32(this.TextBox1.Text);
+            int num2 = Convert.ToInt32(this.TextBox2.Text);
+            
+            int sum = num1 - num2;
+
+            this.Label2.Text = sum.ToString();
+        }
+
+        if (DropDownList1.SelectedValue == "Multiplicacion")
+        {
+            int num1 = Convert.ToInt32(this.TextBox1.Text);
+            int num2 = Convert.ToInt32(this.TextBox2.Text);
+            int sum = num1 * num2;
+            this.Label2.Text = sum.ToString();
+        }
+
+        if (DropDownList1.SelectedValue == "Divisiones")
+        {
+            int num1 = Convert.ToInt32(this.TextBox1.Text);
+            int num2 = Convert.ToInt32(this.TextBox2.Text);
+            int sum = num1 * num2;
+            this.Label2.Text = sum.ToString();
+        }
+
+        // this.Label2.Text = (Convert.ToInt32(TextBox1.Text) + Convert.ToInt32(TextBox2.Text);
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+             
+
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string texto = "";
+        string operador = this.DropDownList2.SelectedItem.Text;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                texto += operador;
+            }
+            texto += "<br />";
+            this.Label4.Text = texto;
+        }
+
+    }
+
+}
